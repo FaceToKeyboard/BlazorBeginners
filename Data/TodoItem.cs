cf1b00afd679c136b57914e3af3096e06ebb5eb5
FaceToKeyboard <fanno.chea@gmail.com>
2022-01-14 14:29:39 -0700

    TodoItem class to store individual todos

---
+++ Data/TodoItem.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorBeginners.Data;
+
+public class TodoItem
+{
+  public string Name { get; set; } = string.Empty;
+  public string Description { get; set; } = string.Empty;
+  public DateTime DueDate { get; set; }
+  public bool IsDone { get; set; }
+
+}
