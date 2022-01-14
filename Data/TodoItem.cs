8b9ce6166364845daede313cff7b2c93cb90e0c4
FaceToKeyboard <fanno.chea@gmail.com>
2022-01-14 16:16:29 -0700

    Support Todo items that may or may not have date and/or time

--- Data/TodoItem.cs
+++ Data/TodoItem.cs
@@ -4,7 +4,8 @@ public class TodoItem
 {
   public string Name { get; set; } = string.Empty;
   public string Description { get; set; } = string.Empty;
-  public DateTime? DueDate { get; set; }
+  public DateOnly? DueDate { get; set; }
+  public TimeOnly? DueTime { get; set; }
   public bool IsDone { get; set; }
 
 }
