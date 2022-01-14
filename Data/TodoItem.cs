905cadcf60190b44f7e925eb338b833317039cba
FaceToKeyboard <fanno.chea@gmail.com>
2022-01-14 15:22:51 -0700

    nullable DueDate in TodoItem

--- Data/TodoItem.cs
+++ Data/TodoItem.cs
@@ -4,7 +4,7 @@ public class TodoItem
 {
   public string Name { get; set; } = string.Empty;
   public string Description { get; set; } = string.Empty;
-  public DateTime DueDate { get; set; }
+  public DateTime? DueDate { get; set; }
   public bool IsDone { get; set; }
 
 }
